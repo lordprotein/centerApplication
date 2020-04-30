22b1be3c24f14f08d21f4d87e2d47352c258d6f1
lordprotein <gamee.logi@gmail.com>
2020-04-30 18:34:19 +0500

    add btn

---
+++ app/src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+
+export const ButtonAddApplication = () => {
+    return (
+        <button className={styles.btnAddApplication}></button>
+    );
+}
