3f480400800662fa1336b9a6562dc95d8d95aef7
lordprotein <gamee.logi@gmail.com>
2020-04-30 13:29:58 +0500

    create redux store

--- app/src/index.js
+++ app/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/App/AppContainer';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers/reducers';
-import App from './containers/App/AppContainer';
+import { store } from './stores/stores';
 
 
 const RenderApp = () => {
-    const store = createStore(reducers);
 
     return (
         <Provider store={store}>
