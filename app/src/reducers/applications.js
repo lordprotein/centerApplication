c7795ffff338c3538aec7636f0d3d20b8e23f05b
lordprotein <gamee.logi@gmail.com>
2020-05-04 21:30:51 +0500

    add reducer for application

--- app/src/reducers/applications.js
+++ app/src/reducers/applications.js
@@ -3,11 +3,42 @@ import * as types from '../constants/actionTypes';
 
 const applications = (state = initialState, action) => {
     switch (action.type) {
-        case types.CREATE_APPLICATION: {
-         
+        case types.SET_APP_LIST: {
+            const { data, data: { appPart } } = state;
+            const { appList } = action;
+
+            return {
+                ...state,
+                data: {
+                    ...data,
+                    appPart: {
+                        ...appPart,
+                        list: appList,
+                        count: appList.length
+                    }
+                }
+            }
         }
+        
         case types.REMOVE_APPLICATION: {
-            
+            const { ID } = action;
+            const { data, data: { appPart, appPart: { list } } } = state;
+
+
+            const numItem = list.findIndex(item => item.ID = ID);
+
+            const newAppList = [...list.slice(0, list[numItem]), ...list.slice(list[numItem + 1])];
+
+            return {
+                ...state,
+                data: {
+                    ...data,
+                    appPart: {
+                        ...appPart,
+                        list: newAppList
+                    }
+                }
+            }
         }
         default: return state;
     }
