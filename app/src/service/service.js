234c6c35505f4a6588880a88e99944f8be3756cb
lordprotein <gamee.logi@gmail.com>
2020-05-07 15:09:47 +0500

    add normalize app

--- app/src/service/service.js
+++ app/src/service/service.js
@@ -28,24 +28,48 @@ class BaseFuncService {
 class Service {
     constructor() {
         this._baseFunc = new BaseFuncService();
+        this._normalizeApp = new NormalizeApp();
     }
-    
+
     //Get
-    
-    getApplItem = (id) => {
-        return this._baseFunc.getResource(`/application/${id}`);
+
+    getApplItem = async (id) => {
+        const res = await this._baseFunc.getResource(`/application/${id}`);
+        return this._normalizeApp.app(res);
     }
 
-    getApplList = (status) => {
-        return this._baseFunc.getResource(`/application/list/${status}`);
+    getApplList = async (status) => {
+        const res = await this._baseFunc.getResource(`/application/list/${status}`);
+        return this._normalizeApp.app(res);
     }
 
-    getAppListFree = () => {
-        return this._baseFunc.getResource(`/application/list/free`);
+    getAppListFree = async () => {
+        const res = await this._baseFunc.getResource(`/application/list/free`);
+        return this._normalizeApp.app(res);
     }
 
     //Post
 }
 
+
+class NormalizeApp {
+    app = data => {
+        return data.map(item => {
+            const { ID, case_num, date, full_name, phone_num, priority, status, task } = item;
+            
+            return {
+                id: ID,
+                caseNum: case_num,
+                date,
+                name: full_name,
+                phone: phone_num,
+                priority,
+                status,
+                task
+            };
+        })
+    }
+}
+
+
 export const service = new Service();
-export const newService = Service;
