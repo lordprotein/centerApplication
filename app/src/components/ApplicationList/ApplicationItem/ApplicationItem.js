7e29f7a7c326e19437d2159b96aa62f47a2ba64e
lordprotein <gamee.logi@gmail.com>
2020-05-08 16:58:18 +0500

    add colord for status

--- app/src/components/ApplicationList/ApplicationItem/ApplicationItem.js
+++ app/src/components/ApplicationList/ApplicationItem/ApplicationItem.js
@@ -5,8 +5,8 @@ import { shortenName } from '../../../service/shortName';
 
 export const ApplicationItem = ({ data }) => {
     const { date, task, name, status } = data;
-    
-    
+
+
     return (
         <div className={styles.row}>
             <div className={styles.title}><input type='checkbox' /></div>
@@ -14,14 +14,27 @@ export const ApplicationItem = ({ data }) => {
             <div className={styles.title}>{date}</div>
             <div className={styles.title}>{shortenName(name)}</div>
             <div className={styles.title}>no name</div>
-            <div className={comboStyle().status}>{status}</div>
+            <div className={chooseColorStatus(status)}>{status}</div>
         </div>
     );
 }
 
 
-function comboStyle() {
-    return {
-        status: `${styles.title} ${styles.status}`,
+// function comboStyle(style) {
+//     const styleList = {
+      
+//     }
+    
+//     return styleList[style];
+// }
+
+function chooseColorStatus(status) {
+    console.log(status)
+    switch (status) {
+        case 'free': return `${styles.title} ${styles.statusFree}`;
+        case 'process': return `${styles.title} ${styles.statusProcess}`;
+        case 'complited': return `${styles.title} ${styles.statusComplited}`;
+
+        default: return;
     }
 }
