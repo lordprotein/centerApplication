85dac628b162fba156e92eb2a7a423f75f98bf79
lordprotein <gamee.logi@gmail.com>
2020-05-17 19:23:24 +0500

    add readExecutersList to server

--- server/controllers/application.controller.js
+++ server/controllers/application.controller.js
@@ -64,6 +64,27 @@ exports.readList = (req, res) => {
 }
 
 
+exports.readExecutersList = (req, res) => {
+    Application.readExecutersList(req, (err, data) => {
+        if (err) {
+            if (err.kind === 'not_found') {
+                res.status(404).send({
+                    message: `Not found names list with id ${req.params.id}.`
+                });
+            }
+            else {
+                res.status(500).send({
+                    message: `Error retrieving Application with id ${req.params.id}`
+                });
+            }
+        }
+        else {
+            res.send(data);
+        }
+    });
+}
+
+
 exports.updatePriority = (req, res) => {
     if (!req.body) {
         res.status(400).send({
--- server/routes/application.route.js
+++ server/routes/application.route.js
@@ -3,12 +3,13 @@ const Application = require('../controllers/application.controller');
 
 module.exports = app => {
     app.get('/application/:id', Application.read);
-    app.get('/application/list/:status', Application.readList);
+    // app.get('/application/list/:status', Application.readList); //TO DELETE
+    app.get('/application/executers/:id', Application.readExecutersList);
 
     app.post('/application', Application.create);
 
     app.put('/application/priority/:id', Application.updatePriority);
-    app.put('/application/status/:id',Application.updateStatus);
+    app.put('/application/status/:id', Application.updateStatus);
 
     app.delete('/application/:id', Application.delete);
 }
