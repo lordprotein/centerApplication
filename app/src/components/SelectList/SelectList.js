7f9f32391c2d84ac92f9d670a3dcdf9996a0b60f
lordprotein <gamee.logi@gmail.com>
2020-05-27 12:54:46 +0500

    change defaultValue to title in Select

--- app/src/components/SelectList/SelectList.js
+++ app/src/components/SelectList/SelectList.js
@@ -2,12 +2,11 @@ import React from 'react';
 import uniqid from 'uniqid';
 
 
-export const SelectList = ({ list, handleSelect, defaultValue, multiple }) => {
-    multiple = !multiple ? false : 'multiple';
+export const SelectList = ({ list, handleSelect, defaultValue, title }) => {
     return (
-        <select onChange={handleSelect} defaultValue={defaultValue} multiple={multiple}>
+        <select onChange={handleSelect} defaultValue={title}>
+            {title && <option disabled>{title}</option>}
             {list.map(item => {
-
                 const { value, title } = item;
                 if (value) {
                     return <option value={value} key={uniqid()}>{title}</option>;
