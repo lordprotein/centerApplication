d2465fcba4f317a19e4466201d095b6d8a1d5145
lordprotein <gamee.logi@gmail.com>
2020-05-29 18:51:15 +0500

    add check cookie

--- app/src/containers/App/AppContainer.js
+++ app/src/containers/App/AppContainer.js
@@ -7,17 +7,23 @@ import { service } from '../../service/service';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as userActions from '../../actions/userActions';
+import { myCookieUser } from '../../service/myCookie';
+import { selectorsUser } from '../../selectors/user';
 
 
 class AppContainer extends Component {
     componentDidMount = () => {
-        const { setExistExecuters } = this.props;
+        console.log('create')
+        const { setExistExecuters, login } = this.props;
 
         service.getExistExecuters().then(list => {
             setExistExecuters(list);
         });
-    }
 
+        const savedUserData = myCookieUser.get();
+        savedUserData && login(savedUserData);
+    }
+    
     render() {
         const PageContainerWithRoutes = withRoutes(PageContainer);
 
@@ -31,11 +37,18 @@ class AppContainer extends Component {
 }
 
 
-const mapDispatchToProps = dispatch => {
-    const { setExistExecuters } = bindActionCreators(userActions, dispatch);
-
-    return { setExistExecuters };
+const mapStateToProps = state => {
+    return {
+        existExecutersList: selectorsUser.existExecuters(state)
+    }
 }
 
 
-export default connect(null, mapDispatchToProps)(AppContainer);
+const mapDispatchToProps = dispatch => {
+    const { setExistExecuters, login } = bindActionCreators(userActions, dispatch);
+
+    return { setExistExecuters, login };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppContainer);
