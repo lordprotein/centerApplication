b668889774c8e3d9f1a29bf48a49db932abb7a1a
lordprotein <gamee.logi@gmail.com>
2020-05-30 14:29:11 +0500

    add check and styles for BtnExit

--- app/src/components/BtnExit/BtnExit.js
+++ app/src/components/BtnExit/BtnExit.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import styles from './BtnExit.module.css';
+
 
 export const BtnExit = ({ handleExit }) => {
     let history = useHistory();
 
     return (
-        <button onClick={() => handleExit(history)}>Выйти</button>
+        <button className={styles.btn} onClick={() => handleExit(history)}>Выйти</button>
     );
 }
