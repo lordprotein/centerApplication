e8bb239ff9599af55a35f99e5d67624cb70d0209
lordprotein <gamee.logi@gmail.com>
2020-06-02 20:29:11 +0500

    review SelectLists

--- app/src/containers/SelectList/SelectListContainer.js
+++ app/src/containers/SelectList/SelectListContainer.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { SelectList } from '../../components/SelectList/SelectList';
 import { withAdmin } from '../../hoc/withRole';
 import { priorityNormalize } from '../../components/ApplicationList/ApplicationItem/formattingFunctions';
+import { selectorsUser } from '../../selectors/user';
+import { store } from '../../stores/stores';
 
 
 class SelectListContainer extends Component {
@@ -25,36 +27,49 @@ class SelectListContainer extends Component {
 export default SelectListContainer;
 
 
-export const SelectListExecutersWithAdmin = ({ existExecutersList, title = '', children }) => {
-    const NewComponent = withAdmin(SelectListContainer);
-    const executersList = existExecutersList.map(({ full_name, ID }) => {
-        return {
-            title: full_name,
-            value: ID
-        }
-    })
+export const SelectListExecuters = ({ existExecutersList, title = '', children }) => {
+    let executersList = [];
+
+    if (existExecutersList) {
+        executersList = existExecutersList.map(({ full_name, ID }) => {
+            return {
+                title: full_name,
+                value: ID
+            }
+        })
+    }
+    else {
+        executersList = selectorsUser.existExecuters(store.getState()).map(({ full_name, ID }) => {
+            return {
+                title: full_name,
+                value: ID
+            }
+        });
+    }
 
     return (
-        <NewComponent
+        <SelectListContainer
             list={executersList}
             title={title}
         >
             {children}
-        </NewComponent>
+        </SelectListContainer>
     );
 }
 
 
-export const SelectListPriorityWithAdmin = ({ handleSelect }) => {
+export const SelectListPriority = ({ handleSelect }) => {
     const { setPriority } = handleSelect;
-    const NewComponent = withAdmin(SelectListContainer);
 
     return (
-        <NewComponent
+        <SelectListContainer
             list={priorityNormalize()}
             title={'Выставить приоритет'}
         >
             {(value) => setPriority(priorityNormalize(value, true))}
-        </NewComponent>
+        </SelectListContainer>
     );
 }
+
+export const SelectListExecutersWithAdmin = withAdmin(SelectListExecuters);
+export const SelectListPriorityWithAdmin = withAdmin(SelectListPriority);
