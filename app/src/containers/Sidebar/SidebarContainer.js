c258269dbb4ec0f430554433e817c11c3435ccab
lordprotein <gamee.logi@gmail.com>
2020-06-10 12:35:13 +0500

    close component without logIn

--- app/src/containers/Sidebar/SidebarContainer.js
+++ app/src/containers/Sidebar/SidebarContainer.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import { Sidebar } from '../../components/Sidebar/Sidebar';
 import MenuListContainer from '../MenuList/MenuListContainer';
 import UserInfoContainer from './UserInfo/UserInfoContainer';
+import { connect } from 'react-redux';
+import { selectorsUser } from '../../selectors/user';
 
 
 class SidebarContainer extends Component {
 
     render() {
+        const { isLogin } = this.props;
+
         return (
             <Sidebar>
-                <UserInfoContainer />
-                <MenuListContainer />
+                {isLogin
+                    ? (
+                        <>
+                            <UserInfoContainer />
+                            <MenuListContainer />
+                        </>
+                    )
+                    :
+                    false}
+
             </Sidebar>
         );
     }
 }
 
-export default SidebarContainer;
+
+const mapStateToProps = state => {
+    return {
+        isLogin: selectorsUser.status(state)
+    }
+}
+
+
+export default connect(mapStateToProps, null)(SidebarContainer);
