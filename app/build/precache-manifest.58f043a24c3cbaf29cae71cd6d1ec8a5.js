7c059f76328272b6f7414c60f3d15af07a657f77
lordprotein <gamee.logi@gmail.com>
2020-06-15 11:21:58 +0500

    build

---
+++ app/build/precache-manifest.58f043a24c3cbaf29cae71cd6d1ec8a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7563574cba6538ad71c62528d372b13a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca79b09caac8dce1dc6d",
+    "url": "/static/css/main.470879d5.chunk.css"
+  },
+  {
+    "revision": "b27674e8fc7f025ff044",
+    "url": "/static/js/2.a5cf21c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a5cf21c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca79b09caac8dce1dc6d",
+    "url": "/static/js/main.6f9b2505.chunk.js"
+  },
+  {
+    "revision": "817d2bf81990a1d39ee2",
+    "url": "/static/js/runtime-main.6537f79a.js"
+  }
+]);
